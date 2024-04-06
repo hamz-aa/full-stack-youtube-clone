076aeee0895c9b182115d47883f2050f36d718c3
Hamza <jhamzaahmed@gmail.com>
2024-04-06 21:29:57 +0500

    setting up routes and controllers for user

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,6 +1,35 @@
 import express from "express";
-import {} from "../controllers/user.js";
+import {
+  deleteUser,
+  dislike,
+  getUser,
+  like,
+  subscribe,
+  unsubscribe,
+  update,
+} from "../controllers/user.js";
 
 const router = express.Router();
+
+// UPDATE
+router.put("/:id", update);
+
+// DELETE
+router.delete("/:id", deleteUser);
+
+// GET
+router.get("/find/:id", getUser);
+
+// SUBSCRIBE
+router.put("/sub/:id", subscribe);
+
+// UNSUBSCRIBE
+router.put("/unsub/:id", unsubscribe);
+
+// LIKE
+router.put("/like/:id", like);
+
+// DISLIKE
+router.put("/dislike/:id", dislike);
 
 export default router;
