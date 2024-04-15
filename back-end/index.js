2aa1d4d483da15d390d120e82407d6dd37c5c572
Hamza <jhamzaahmed@gmail.com>
2024-04-15 20:55:10 +0500

    added cors

--- back-end/index.js
+++ back-end/index.js
@@ -6,6 +6,7 @@ import videoRoute from "./routes/videos.js";
 import commentRoute from "./routes/comments.js";
 import authRoute from "./routes/auth.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -21,6 +22,7 @@ const connect = () => {
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
